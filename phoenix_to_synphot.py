2ac5a345a9a48a8955eb387f50c4745dcf2437c2
James White <jwhite@stsci.edu>
2017-08-23 16:57:34 -0400

    first iteration of the rewritten version of the old phoenix code

---
+++ phoenix_to_synphot.py
@@ -0,0 +1,268 @@
+import os
+from astropy.io import fits
+import numpy as np
+import pysynphot as pysyn
+from datetime import date
+import glob
+import dask
+import argparse
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def find_models(model_directory):
+    """ Find all models in a given directory"""
+    print('\nFinding models..\n')
+
+    path = os.path.join(model_directory, 'lte*')
+    models = glob.glob(path)
+    for f in models:
+        loc, name = os.path.split(f)
+        print('\t{}'.format(name))
+
+    return models
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def sort_by_temp_metal(list_of_model_files):
+    """ Given a list of models, sort them by temperature. The temperature is given as a part of the file naming
+        convention
+    """
+    print('\nSorting files by Temperature and Metallicity')
+    # Create a dictionary that will contain a list of files for each temperature keyword
+    files_per_tempmetal = {}
+
+    keys = []
+    for f in list_of_model_files:
+        name = os.path.split(f)[-1]
+
+        temp = name[3:6]
+        metal = name[10:14]
+        keys.append((temp, metal))
+
+    for t, m in keys:
+        key = '_'.join([t, m])
+        files_per_tempmetal[key] = [model for model in list_of_model_files if (t in model and m in model)]
+        print('\nFound {} models for {}\{} temperature\metallicity values'.format(len(files_per_tempmetal[key]), t, m))
+
+    return files_per_tempmetal
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def generate_fits(temperature_metallicity_key):
+    """ Generate a fits file name and primary header based on the key generated in sort_by_temp_metal
+    """
+    print('\nCreating new file')
+
+    temperature, metallicity = temperature_metallicity_key.split('_')[0], temperature_metallicity_key.split('_')[1]
+    temperature += '00'
+
+    # Metallicity sign
+    if '-' in metallicity:
+        sign = 'm'
+    else:
+        sign = 'p'
+
+    # Construct filename and corresponding HDU object
+    output_file = 'phoenix' + sign + metallicity + '_' + temperature + '.fits'
+
+    today = date.today()
+    date_string = '{}-{}-{}'.format(today.year, today.month, today.day)
+
+    pri_hdu = fits.PrimaryHDU()
+    header = pri_hdu.header[0]
+    header['FILENAME'] = (output_file, 'Name of file')
+    header['MAPKEY'] = ('phoenix', 'Mapping identifier for filetype')
+    header['CONTACT'] = ('J. White/M. McMaster', 'ReDCaT Team Deputy/Lead')
+    header['CREATED'] = (date_string, 'Date of file creation')
+    header['DESCRIP'] = 'Phoenix Models BT-settl Allard et al. 03, 07, 09'
+    header['FILE_TYP'] = ('Atmosphere Grid Model', 'Type of file')
+    header['SYSTEMS'] = ('ETC, CDBS, PYSYNPHOT', 'Systems that will use this filetype')
+    header['REASON'] = 'Delivered to support JWST'
+    header['TEFF'] = (temperature, 'Effective temperature in K')
+    header['LOG_Z'] = (metallicity, 'Log of stellar metallicity')
+    header['FLUXUNT'] = ('Flambda', 'erg/cm^2/s/Angstrom')
+    header['COMMENT'] = '= Files translated to CDBS format by J. White'
+
+    return pri_hdu, output_file
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def get_data(model_file):
+    """ Recover information such as wavelength, flux and log(G) from the model file
+    """
+    model_path, model_name = os.path.split(model_file)
+
+    expected_log_gs = ['0.0', '0.5', '1.0', '1.5', '2.0', '2.5', '3.0', '3.5', '4.0', '4.5', '5.0', '5.5']
+
+    log_g = model_name[7:10]  # log_g value is given in the naming converntion
+
+    if log_g not in expected_log_gs:
+        raise TypeError('Non-standard or mis-named file')
+
+    wavelength = np.array([], dtype='float64')
+    flux = np.array([], dtype='float64')
+    n_rows_limit = 500000  # Maximum number of rows allowed in conversion from model files to synphot fits file
+    with open(model_file) as m:
+        for i, line in enumerate(m):
+            if i + 1 < n_rows_limit:
+                row = line.split()
+
+                # Columns in order: wavelenght, flux
+                wave = float(row[0].replace('D', 'E'))  # Given in scientific notation with a D instead of E
+                f = 10.0**float(row[1].replace('D', 'E')) * 1.0e-8  # convert to the correct units
+
+                # Break if wave[i] - wave[i-1] becomes negative
+                if i == 0:
+                    pass
+                elif i > 0:
+                    step = wave - wavelength[i-1]
+                    if step < 0:
+                        break
+
+                wavelength = np.append(wavelength, [wave])
+                flux = np.append(flux, [f])
+
+    return log_g, wavelength, flux
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def create_synphot(wavelengths, wave_array, flux_array):
+    """ From the wavelength and flux data obtained from the phoenix model files, build the column data for the
+        primary HDU objects that will become the synphot files
+    """
+
+    spectrum = pysyn.ArraySpectrum(wave=wave_array, flux=flux_array, waveunits='angstroms', fluxunits='flam')
+    simulated_flux = spectrum.sample(wavelengths)
+
+    return simulated_flux
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def create_fits(hdu, outname, loggs, wave_band_array, syn_fluxes):
+    """ From the sorted list of phoenix model files extract the necessary information and produce synphot fits
+        files.
+    """
+    # dictionary items
+    # log_g file naming convention: (fits column name, fits header key)
+
+    log_names = {'0.0': ('g00', 'LOGG1'),
+                 '0.5': ('g05', 'LOGG2'),
+                 '1.0': ('g10', 'LOGG3'),
+                 '1.5': ('g15', 'LOGG4'),
+                 '2.0': ('g20', 'LOGG5'),
+                 '2.5': ('g25', 'LOGG6'),
+                 '3.0': ('g30', 'LOGG7'),
+                 '3.5': ('g35', 'LOGG8'),
+                 '4.0': ('g40', 'LOGG9'),
+                 '4.5': ('g45', 'LOGG10'),
+                 '5.0': ('g50', 'LOGG11'),
+                 '5.5': ('g55', 'LOGG12')}
+
+    columns = []
+    wavelength_column = fits.Column(name='WAVELENGTH', format='D', unit='ANGSTROM', array=wave_band_array)
+    columns.append(wavelength_column)
+
+    for logg, flux in zip(loggs, syn_fluxes):
+        column_name = log_names[logg][0]
+        header_keyword = log_names[logg][1]
+
+        if not flux.any():
+            hdu.update(header_keyword, '-999')
+        else:
+            hdu.update(header_keyword, logg)
+
+        column = fits.Column(name=column_name, format='D', unit='FLAM', array=flux)
+        columns.append(column)
+
+    fits_columns = fits.ColDefs(columns)
+    table = fits.new_table(fits_columns)
+    hdulist = fits.HDUList([hdu])
+    hdulist.append(table)
+
+    hdulist.writeto(outname)
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def patch_logg(log_g_list, wavelengths_list, fluxes_list):
+    """ Fill in missing data with zeroes
+    """
+    expected_loggs = ['0.0', '0.5', '1.0', '1.5', '2.0', '2.5', '3.0', '3.5', '4.0', '4.5', '5.0', '5.5']
+
+    for lg in expected_loggs:
+        if lg not in log_g_list:
+            log_g_list.append(lg)
+            wavelengths_list.append(np.zeros_like(wavelengths_list[0]))
+            fluxes_list.append(np.zeros_like(fluxes_list[0]))
+
+    return log_g_list, wavelengths_list, fluxes_list
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def make_synphot_files(phoenix_directory):
+    """ Put everything together.. from the list of phoenix model files construct synphot fits files
+    """
+    files = find_models(phoenix_directory)
+    sorted_dictionary = sort_by_temp_metal(files)
+
+    # Wave bands
+    blue_band = np.logspace(1.0, 3.695, num=1000, endpoint=True, base=10.0)
+    green_band = np.logspace(3.696, 5.455, num=3000, endpoint=True, base=10.0)
+    red_band = np.logspace(5.456, 6.998, num=1000, endpoint=True, base=10.0)
+    wavelengths = np.hstack((blue_band, green_band, red_band))
+
+    for key, models in sorted_dictionary.items():
+        hdu, output_filename = generate_fits(key)
+
+        waves = []
+        fluxes = []
+        log_gs = []
+        for model in models:
+            log_g, wave, flux = dask.delayed(get_data)(model)
+
+            waves.append(wave)
+            fluxes.append(flux)
+            log_gs.append(log_g)
+
+        patched_loggs, patched_waves, patched_fluxes = dask.delayed(patch_logg)(log_gs, waves, fluxes)
+        lgs, wave_dat, flux_dat = dask.compute(patched_loggs, patched_waves, patched_fluxes)
+
+        synphot_fluxes = []
+        for wave, flux in patched_waves, patched_fluxes:
+            syn_f = dask.delayed(create_synphot)(wavelengths, wave, flux)
+            synphot_fluxes.append(syn_f)
+
+        synphot_results = dask.compute(*synphot_fluxes)
+
+        create_fits(hdu, output_filename, lgs, wavelengths, synphot_results)
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def convert_phoenix_to_synphot():
+    """ handles argument parsing and execution
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--model_directory',
+                        '-d',
+                        action='store',
+                        default=os.getcwd(),
+                        help='Directory where the model files are located. Default is the current working directory')
+
+    args = parser.parse_args()
+
+    make_synphot_files(args.model_directory)
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    convert_phoenix_to_synphot()
