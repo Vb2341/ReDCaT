7377f1730ced2fc8b3e145f0d9c75b295e5a9c53
James White <jwhite@stsci.edu>
2017-09-20 12:23:15 -0400

    copy pasta error
    shlex.split was being called on the wrong string

--- jwst_etc_check.py
+++ jwst_etc_check.py
@@ -443,7 +443,7 @@ if __name__ == '__main__':
     move_to_pandeia(files, instrument, destination)
     if options.m:
         move_code = 'python /grp/redcat/SCRIPTS/ReDCaT/move_files.py'
-        move_cmd = shlex.split(uniqname)
+        move_cmd = shlex.split(move_code)
 
         with subprocess.Popen(move_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
             out_dat, out_err = p.communicate()
